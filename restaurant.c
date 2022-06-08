ad0769d11a65828c033d2c2b930fcc576ba4325f
leonardosilvamelosantos <57964391+leonardosilvamelosantos@users.noreply.github.com>
2022-06-07 21:59:40 -0300

    Update restaurant.c

--- restaurant.c
+++ restaurant.c
@@ -162,7 +162,7 @@ void cadastroP(int cod, int pos){ //Cadastro das pessoas
        //printf("\nDigite seu Telefone: ");*********rio
        //gets(log[pos].telefone);          *********.
         log[pos].vazio = 1;
-        printf("\nDigite enter para efetuar novo cadastro ou qualquer outra tecla para volar ao menu principal");
+        printf("\nDigite enter para efetuar novo cadastro ou qualquer outra tecla para voltar ao menu principal");
         scanf("%d", &opt);
         opt ==1;
         getchar();  
@@ -216,33 +216,113 @@ void cadastroPedido(){ //Cadastro dos pedidos
                 do{
                 printf("\nEscolha o seu pedido: "); //PEDIDOS MERAMENTE ILUSTRATIVOS, COLOQUEI SÓ PARA VER SE CONSEGUIA ANEXAR A 1 CADATRO MAS NÃO CONSEGUI.
                 printf("\n1- Sanduba esperto");
-                printf("\n2- Ceasar Salad");
+                printf("\n2- Salada");
                 printf("\n3- Raviolli\n:");
                 scanf("%d", &OpcaoPedido);
                 if(OpcaoPedido == 1){
                     strcpy(log[i].clientePed.namePed, "Sanduba esperto");
                     printf("\nVoce escolheu %s, seu pedido foi adicionado ao seu cadastro.",log[i].clientePed.namePed);
-                    printf("\nPressione 1 para continuar pedindo ou 2 para volar ao menu principal: ");
+                    printf("\nPressione 1 para continuar pedindo ou 2 para voltar ao menu principal: ");
                     scanf("%d", &Option);
-                    i++;
+                    i++;}
                 }
                 else if(OpcaoPedido == 2){
-                    strcpy(log[i].clientePed.namePed, "Ceasar Salad");
+                    strcpy(log[i].clientePed.namePed, "Salada");
                     printf("\nVoce escolheu %s, seu pedido foi adicionado ao seu cadastro.", log[i].clientePed.namePed);
-                    printf("\nPressione 1 para continuar pedindo ou 2 para volar ao menu principal: ");
+                    printf("\nPressione 1 para continuar pedindo ou 2 para voltar ao menu principal: ");
                     scanf("%d", &Option);
                     i++;
                    }
                    else if(OpcaoPedido == 3){
                     strcpy(log[i].clientePed.namePed, "Raviolli");
                     printf("\nVoce escolheu %s, seu pedido foi adicionado ao seu cadastro.", log[i].clientePed.namePed);
-                    printf("\nPressione 1 para continuar pedindo ou 2 para volar ao menu principal: ");
+                    printf("\nPressione 1 para continuar pedindo ou 2 para voltar ao menu principal: ");
                     scanf("%d", &Option);
                     i++;
                 }
-    }while(Option == 1){
-    printf("\033[H\033[J");
     }
+        
+        struct filaElementos
+{
+ int comeco, fim;
+ int elementos[m];
+};
+
+void iniciarFila(struct filaElementos *fila)
+{
+fila->comeco = 0;
+fila->fim = -1;
+}
+
+void inserir(struct filaElementos *fila, int namePed)
+{
+    if(fila->fim == m - 1)
+    printf("Fila está lotada, aguarde... \n");
+else
+{
+    fila->fim++;
+    fila->elementos[fila->fim] = x;
+}
+}
+void consultar(struct filaElementos *fila)
+{
+int i = 0;
+    if(fila->fim < fila->comeco)
+    printf("fila Vazia \n");
+else
+{
+while(i < m)
+{
+    printf(" Listando todos os pedidos  = %d \n", fila->elementos[i]);
+i++;
+}
+}
+}
+
+void consultarComecoFila(struct filaElementos *fila)
+{
+int i = 0;
+printf("O primeiro pedido em fila : %d \n", fila->elementos[fila->comeco]);
+}
+
+void remover(struct filaElementos *fila){
+    if(fila->fim < fila->comeco)
+    printf("fila Vazia\n");
+else
+fila->comeco++;
+}
+void esvaziar(struct filaElementos *fila){
+    if(fila->fim < fila->comeco)
+    printf("fila Vazia\n");
+else
+{
+int i;
+for (i = 0; i < m; ++i)
+{
+fila->fim--;
+}
+}
+}
+int main()
+{
+int i;
+struct filaElementos fila;
+int x;
+iniciarFila(&fila);
+for(i = 0; i < m; i++)
+{
+        printf("Informe o código da opção desejada = \n");
+        scanf("%d", &x);
+        inserir(&fila, x);
+}
+    remover(&fila);
+    consultar(&fila);
+    esvaziar(&fila);
+    consultar(&fila);
+    consultarComecoFila(&fila);
+return 0;
+}
+   
    
 
    
@@ -281,7 +361,7 @@ void consultaCod (void) // CONSULTAR 1 CADASTRADO QUALQUER VIA CÓDIGO DADO POR 
         cont++;
 
         if ( cont > 200 ){
-            printf("\nCodigo nao encontrado, pressione enter para volar ao menu principal\n");
+            printf("\nCodigo nao encontrado, pressione enter para voltar ao menu principal\n");
             getchar();
             printf("\033[H\033[J");
         }
